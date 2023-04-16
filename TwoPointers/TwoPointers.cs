6b6a1442db5598af1850c2d5f2d9ca3d475e132e
Alex42 <alexcrazy42@mail.ru>
2023-04-16 21:25:59 +0300

    16.04

---
+++ TwoPointers/TwoPointers.cs
@@ -0,0 +1,26 @@
+﻿
+
+int[] list = new int[4] { 2, 7, 11, 15 };
+
+int target = 9;
+
+int left = 0;
+int right = list.Length - 1;
+
+while (left < right)
+{
+    int sum = list[left] + list[right];
+    if (sum == target)
+    {
+        Console.WriteLine($"indices of elements giving in summing target: {left}, {right} ");
+        break;
+    }
+    if (sum > target)
+    {
+        right--;
+    }
+    if (sum < target)
+    {
+        left++;
+    }
+}

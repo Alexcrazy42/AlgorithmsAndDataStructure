72f70d722b455beed343a4ddde11db9f150aaacc
Alex42 <alexcrazy42@mail.ru>
2023-05-02 19:48:10 +0300

    add bubble sort

---
+++ Algorithms/Sortings/Primitive/BubbleSort/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next() % 100;
+}
+
+Console.WriteLine("Array before bubbleSort: ");
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = 0; j < array.Length - i - 1; j++)
+    {
+        if (array[j] > array[j+1])
+        {
+            (array[j], array[j+1]) = (array[j+1], array[j]);
+        }
+    }
+}
+
+Console.WriteLine("Array after bubbleSort: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}

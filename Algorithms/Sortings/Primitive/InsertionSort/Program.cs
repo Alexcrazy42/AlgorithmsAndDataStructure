83571d216fadabd968ea577d90f60efd058b276c
Alex42 <alexcrazy42@mail.ru>
2023-05-02 21:37:04 +0300

    add insertionSort

---
+++ Algorithms/Sortings/Primitive/InsertionSort/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next() % 100;
+}
+
+Console.WriteLine("Array before insertionSort: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+
+
+int mid = 0;
+while (array[mid] < array[mid+1])
+{
+    mid++;
+}
+
+
+for (int i = mid+1; i < array.Length; i++)
+{
+    int x = array[i];
+    int j = i;
+    while (j > 0 && array[j-1] > x)
+    {
+        array[j] = array[j-1];
+        j--;
+    }
+    array[j] = x;
+}
+
+Console.WriteLine("Array after insertionSort: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}

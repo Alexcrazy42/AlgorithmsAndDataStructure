a0a2c0f3e55960a4bf8bb6d6213a2f093b547530
Alex42 <alexcrazy42@mail.ru>
2023-05-02 21:09:11 +0300

    add selectionSort

--- Algorithms/Sortings/Primitive/SelectionSort/Program.cs
+++ Algorithms/Sortings/Primitive/SelectionSort/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next() % 100;
+}
+
+Console.WriteLine("Array before selectionSort: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    int min = i;
+    for (int j = i+1; j < array.Length; j++)
+    {
+        if (array[j] < array[min])
+        {
+            min = j;
+        }
+    }
+    (array[i], array[min]) = (array[min], array[i]);
+
+}
+
+Console.WriteLine("Array after selectionSort: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
